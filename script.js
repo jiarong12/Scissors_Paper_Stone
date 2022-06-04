8476ceafc7f817fead986ab9637993f1e45b3528
Jia Rong <jiarong1202@gmail.com>
2022-06-05 00:48:46 +0800

    Ver 2

--- script.js
+++ script.js
@@ -1,59 +1,78 @@
-var player = 0;
+var playerChoice = 0;
 var comPlayer = 0;
 var draw = 0;
 var username = ``;
+var total = 0;
+
+var showStats = function () {
+  var playerStats = (playerChoice * 100) / total;
+  var comStats = (comPlayer * 100) / total;
+  var drawStats = (draw * 100) / total;
+  return `Win : ${playerStats.toFixed(1)}% <br> Loss : ${comStats.toFixed(
+    1
+  )}% <br> Draw : ${drawStats.toFixed(1)}% `;
+};
 
 // random number generator
-var randomNum = function( max ){
-  return Math.floor( Math.random() * max ) + 1 ;
-}
+var randomNum = function (max) {
+  return Math.floor(Math.random() * max) + 1;
+};
 var REPLAY_INSTRUCTIONS =
   'Now you can type "scissors" "paper" or "stone" to play another round!';
 
 // computer random generate output
-var comOutput = function(){
-  var numOfPlays = 3 ;
-  var num = randomNum( numOfPlays ) ;
-  if ( num == 1 ){
-    return 'scissors' ;
+var comOutput = function () {
+  var numOfPlays = 3;
+  var num = randomNum(numOfPlays);
+  if (num == 1) {
+    return "scissors";
   }
-  if( num == 2 ){
-    return 'paper' ;
+  if (num == 2) {
+    return "paper";
   }
-  return 'stone' ;
-}
+  return "stone";
+};
 
-var gameOutcome = function( player , com ){
-  if ( player == com ){
+var gameOutcome = function (player, com) {
+  if (player == com) {
     draw += 1;
-    return 'Draw!' ;
+    return "Draw!";
   }
-  if (( player == 'stone' && com == 'scissors') || ( player == 'scissors' && com == 'paper' ) || ( player == 'paper' && com == 'stone' )){
-    player += 1;
-    return 'You Win!' ;
-  }
-  else{
-    comPlayer += 1 ;
+  if (
+    (player == "stone" && com == "scissors") ||
+    (player == "scissors" && com == "paper") ||
+    (player == "paper" && com == "stone")
+  ) {
+    playerChoice += 1;
+    return "You Win!";
+  } else {
+    comPlayer += 1;
     return "You Lose!";
   }
-  
-}
+};
 
-var validityCheck = function ( input ){
-  if ( input == 'stone' || input == 'scissors' || input == 'paper'){
-    return 1 ;
+var validityCheck = function (input) {
+  if (input == "stone" || input == "scissors" || input == "paper") {
+    return 1;
   }
-  return 0 ;
-}
+  return 0;
+};
 
-var game = function( input ){
-  var check = validityCheck( input ) ;
-  if ( check == 0 ){
-    return 'Invalid input! Please key in (scissors/stone/paper)';
+var game = function (input) {
+  if (username == ``) {
+    username = input;
+    return `Hi ${username}! Please enter either "scissors","paper" or "stone" to begin!`;
   }
-  var com = comOutput() ;
-  return `Computer chose ${com} <br><br> You chose ${input} <br></br> ${gameOutcome( input , com )} <br><br> ${REPLAY_INSTRUCTIONS}` ; 
-}
+  total += 1;
+  var check = validityCheck(input);
+  if (check == 0) {
+    return "Invalid input! Please key in (scissors/stone/paper)";
+  }
+  var com = comOutput();
+  var outcome = gameOutcome(input, com);
+  console.log(playerChoice);
+  return `Computer chose ${com} <br><br> You chose ${input} <br></br> ${outcome} <br><br> ${showStats()} <br><br> ${REPLAY_INSTRUCTIONS}`;
+};
 
 var main = function (input) {
   return game(input);
